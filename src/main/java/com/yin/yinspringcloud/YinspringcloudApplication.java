900e4df14adb9e58a7ddc0affe7083e4f408316b
yinzhenzhao <yinpursue@163.com>
2019-07-15 14:37:16 +0800

    first commit

---
+++ src/main/java/com/yin/yinspringcloud/YinspringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.yin.yinspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinspringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinspringcloudApplication.class, args);
+	}
+
+}
