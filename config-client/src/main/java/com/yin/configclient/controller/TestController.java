e8ec70e0723811cc15e416e6bbbd5866999d3af5
yinzhenzhao <yinpursue@163.com>
2019-10-12 15:45:22 +0800

    配置中心server端和client端

---
+++ config-client/src/main/java/com/yin/configclient/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.yin.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping(value = "/from")
+    public String from(){
+        return from;
+    }
+}
--- hello-service/src/main/java/com/yin/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/yin/helloservice/HelloServiceApplication.java
@@ -3,9 +3,11 @@ package com.yin.helloservice;
 import com.yin.controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackageClasses = HelloController.class)
