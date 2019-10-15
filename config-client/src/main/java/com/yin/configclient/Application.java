7fd64f5fc016ea6e8d86565bc17639d82295ac65
yinzhenzhao <yinpursue@163.com>
2019-10-15 13:34:38 +0800

    1、配置中心注册到注册中心，客户端从注册中心发现配置中心，拉取配置 2、配合actutor,发送post请求，实现动态刷新配置

--- config-client/src/main/java/com/yin/configclient/Application.java
+++ config-client/src/main/java/com/yin/configclient/Application.java
@@ -3,7 +3,11 @@ package com.yin.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@EnableDiscoveryClient
+@RefreshScope
 @SpringBootApplication
 public class Application {
 
