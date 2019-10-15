c42f59ddec3083598224727053e26380fe39dda4
yinzhenzhao <yinpursue@163.com>
2019-10-15 16:18:01 +0800

    1、rabbitmq消息发送与接收 2、整合springcloud bus 用于动态刷新

---
+++ rabbit-hello/src/main/java/com/yin/rabbithello/RabbitHelloApplication.java
@@ -0,0 +1,13 @@
+package com.yin.rabbithello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitHelloApplication.class, args);
+    }
+
+}
